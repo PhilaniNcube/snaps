ea9b58db9b9b8db05e55a63b7f27b9ef7dfdc1b0
Philani Ncube <ncbphi001@gmail.com>
2025-05-24 19:51:41 +0200

    Refactor upload page to include UploadPhotoForm component and remove unused AddNewSchool component

--- app/(dashboard)/dashboard/upload/page.tsx
+++ app/(dashboard)/dashboard/upload/page.tsx
@@ -1,3 +1,4 @@
+import UploadPhotoForm from "@/components/photos/upload-photo-form";
 import AddNewSchool from "@/components/schools/add-new-school";
 import React from "react";
 
@@ -6,9 +7,9 @@ const UploadPage = () => {
     <div>
       <h1 className="text-2xl font-bold mb-4">Upload Page</h1>
       <p>This is the upload page where you can manage file uploads.</p>
-      {/* Add your upload form or components here */}
-      <AddNewSchool />
-      {/* You can also add other components related to uploads */}
+      <div className="mt-6 flex flex-col lg:flex-row">
+        <UploadPhotoForm />
+      </div>
     </div>
   );
 };
--- components/photos/upload-photo-form.tsx
+++ components/photos/upload-photo-form.tsx
@@ -1,9 +1,31 @@
 "use client";
 
+import {
+  Dropzone,
+  DropzoneContent,
+  DropzoneEmptyState,
+} from "@/components/dropzone";
+import { useSupabaseUpload } from "@/hooks/use-supabase-upload";
 import React from "react";
 
 const UploadPhotoForm = () => {
-  return <div>UploadPhotoForm</div>;
+  const props = useSupabaseUpload({
+    bucketName: "school-photos",
+    allowedMimeTypes: ["image/*"],
+    maxFiles: 2,
+    maxFileSize: 1000 * 1000 * 10, // 10MB,
+  });
+
+  return (
+    <div>
+      <div className="w-[500px]">
+        <Dropzone {...props}>
+          <DropzoneEmptyState />
+          <DropzoneContent />
+        </Dropzone>
+      </div>
+    </div>
+  );
 };
 
 export default UploadPhotoForm;
